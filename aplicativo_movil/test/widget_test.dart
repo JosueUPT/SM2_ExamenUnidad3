78393a58b712cbf3a47252b4be1094a09eb24692
JosueUPT <jaechambilla@upt.pe>
2024-12-06 19:08:13 -0500

    v6

--- aplicativo_movil/test/widget_test.dart
+++ aplicativo_movil/test/widget_test.dart
@@ -1,30 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:aplicativo_movil/main.dart';
+import 'package:aplicativo_movil/main.dart'; // Ajusta el import si es necesario
+import 'package:aplicativo_movil/views/login_page.dart'; // Ajusta el import si es necesario
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Verificar que la página inicial es LoginPage',
+      (WidgetTester tester) async {
+    // Construir el widget principal de la aplicación.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verificar que el título o un elemento único de LoginPage está presente.
+    expect(find.byType(LoginPage), findsOneWidget);
   });
 }
